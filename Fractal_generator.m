304a4abbfb4c352e21beb4b329e3928bc1a8aafb
Andrei Dumitrescu <andrei_dumitrescu_96@yahoo.com>
2020-03-30 21:46:16 +0300

    added GUI

---
+++ Fractal_generator.m
@@ -0,0 +1,102 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% DUMITRESCU ANDREI 
+%%% PCSAM 1 - TFAM
+%%% March 2020
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Fractal generator 
+%%% using iterative function
+%%% systems.
+%%%
+%%% Existing fractals:
+%%% Koch,
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%%%%%%%%%% Bootstrap
+% fresh start
+clear all;
+clc;
+% start GUI
+fig = openfig('controll.fig')
+%%%%%%%%%%%%
+
+%%%%%%%%%%%% Fractal selection
+% wait for user input
+while exist('selected_fractal','var') ~= 1
+    uiwait(fig);
+end
+%%%%%%%%%%%%
+
+%%%%%%%%%%%% Initial setup & load
+n=8;
+autoplay = 1;
+switch selected_fractal
+    case 'Koch'
+        Koch
+    case 'Heighway'
+        Heighway
+    case 'Sierpinsky'
+        Sierpinski
+end
+
+
+%creating the iteratinon loop
+for curr_iteration=2:n
+
+    prev_iteration = curr_iteration-1;
+    %define a new starting point for a new iteration
+    x{curr_iteration} = [0
+                         0];
+
+    %for each previous point compute the new points using the 
+    %Iterative Function System
+    nr_of_existing_points = size(x{prev_iteration},2);
+
+    %loop through each IF
+    for f=1:size(IFS,2)
+        %pass all previous points through IF to obtain
+        %the new points
+
+        for curr_point=2:nr_of_existing_points
+            %set the matrix for the current point coordinates
+            curr_coord = x{prev_iteration}(:,curr_point);
+
+            new_coord = IFS{f} * curr_coord + offset(:,f);
+            x{curr_iteration} = [x{curr_iteration} new_coord];
+        end
+    end
+end
+
+while 1
+    if(autoplay == 1)
+        for i=1:size(x,2)
+            figure(1);
+            clf;
+            xlim([-0.2 1.2]);
+            ylim([-0.2 0.4]);
+            line(x{i}(1,:),x{i}(2,:));
+            pause(.2);
+            if(autoplay == 0)
+                break
+            end
+        end
+        for i=size(x,2):-1:1
+            figure(1);
+            clf;
+            xlim([-0.2 1.2]);
+            ylim([-0.2 0.4]);
+            line(x{i}(1,:),x{i}(2,:));
+            pause(.2);
+            if(autoplay == 0)
+                break
+            end
+        end
+    else
+        figure(1);
+        clf;
+        xlim([-0.2 1.2]);
+        ylim([-0.2 0.4]);
+        line(x{n}(1,:),x{n}(2,:));
+        uiwait(fig);
+    end
+end
---
+++ controll.m
@@ -0,0 +1,143 @@
+function varargout = controll(varargin)
+% CONTROLL MATLAB code for controll.fig
+%      CONTROLL, by itself, creates a new CONTROLL or raises the existing
+%      singleton*.
+%
+%      H = CONTROLL returns the handle to a new CONTROLL or the handle to
+%      the existing singleton*.
+%
+%      CONTROLL('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in CONTROLL.M with the given input arguments.
+%
+%      CONTROLL('Property','Value',...) creates a new CONTROLL or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before controll_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to controll_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help controll
+
+% Last Modified by GUIDE v2.5 30-Mar-2020 21:34:39
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @controll_OpeningFcn, ...
+                   'gui_OutputFcn',  @controll_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before controll is made visible.
+function controll_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to controll (see VARARGIN)
+
+% Choose default command line output for controll
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes controll wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = controll_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+selected_fractal = 'Koch';
+assignin('base','selected_fractal', selected_fractal);
+uiresume();
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+selected_fractal = 'Heighway';
+assignin('base','selected_fractal', selected_fractal);
+uiresume();
+
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+selected_fractal = 'Sierpinski';
+assignin('base','selected_fractal', selected_fractal);
+uiresume();
+
+% --- Executes on slider movement.
+function slider1_Callback(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'Value') returns position of slider
+%        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
+val=round(hObject.Value);
+hObject.Value=val;
+assignin('base','n', val);
+uiresume();
+
+% --- Executes during object creation, after setting all properties.
+function slider1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to slider1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: slider controls usually have a light gray background.
+if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor',[.9 .9 .9]);
+end
+
+
+% --- Executes during object creation, after setting all properties.
+function pushbutton1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes on button press in checkbox1.
+function checkbox1_Callback(hObject, eventdata, handles)
+% hObject    handle to checkbox1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+assignin('base','autoplay', hObject.Value);
+uiresume();
+
+% Hint: get(hObject,'Value') returns toggle state of checkbox1
